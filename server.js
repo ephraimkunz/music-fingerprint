247d143be6dd741232b09d682dd3209e2366283e
System <none>
2017-08-26 22:31:50 +0000

    Fetch paginated saved tracks

--- server.js
+++ server.js
@@ -55,19 +55,36 @@ app.get("/isLoggedIn", function(request, response) {
 });
 
 app.get("/userSavedTracks", function(request, response) {
-  getSavedTracks(50, 0)
+  var promises = [];
+  const pageSize = 50;
+  getSavedTracks(pageSize, 0)
     .then(function(data) {
-      console.log(JSON.stringify(data.body.items));
-      response.send(data.body.items.map(function(item) {
+      var total = data.body.total;
+      var nextPage = 1; // Index of the page we are about to fetch
+      total -= pageSize; // Account for the page we just fetched
+      while(total > 0) {
+        promises.push(getSavedTracks(pageSize, nextPage * pageSize));
+        total -= pageSize;
+        nextPage ++;
+      }
+    
+      return Promise.all([data].concat(promises));
+  }).then(function(data) {
+    let items = [];
+    data.forEach(function(piece) {
+      items = items.concat(piece.body.items.map(function(item) {
         return item.track;
       }));
+    });
+    response.send(items);
   }, function(error) {
       console.log(JSON.stringify(error));
   });
 });
 
 function getSavedTracks(limit, skip) {
-  return spotifyApi.getMySavedTracks({limit: limit, skip: skip});
+  console.log('Limit: ' + limit + ', skip: ' + skip);
+  return spotifyApi.getMySavedTracks({limit: limit, offset: skip});
 }
 
 app.get("/callback", function(request, response) {
@@ -80,16 +97,6 @@ app.get("/callback", function(request, response) {
         response.redirect('/');
     })
   }
-});
-
-app.get("/search", function (request, response) {
-  spotifyApi.searchTracks(request.query.query)
-  .then(function(data) {
-    console.log(data.body);
-    response.send(data.body);
-  }, function(err) {
-    console.log(err)
-  });
 });
 
 app.get("/features", function (request, response) {
